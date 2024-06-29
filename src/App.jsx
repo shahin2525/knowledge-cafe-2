8bcf4c8249cb7e02494292c0b7f451cf7f96b52f
shahin2525 <shahin451999@gmail.com>
2024-06-29 02:06:38 -0700

    add profile img

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header></Header>
     </>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import profile from "../../assets/images/profile.png";
+const Header = () => {
+  return (
+    <div>
+      <h1>Knowledge Cafe</h1>
+
+      <img src={profile} alt="profile" />
+    </div>
+  );
+};
+
+export default Header;
